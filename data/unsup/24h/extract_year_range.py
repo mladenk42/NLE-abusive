9d1c12482df8ac603154f22bae1ee60599773fa2
Mladen Karan <m.karan@qmul.ac.uk>
2021-09-09 16:46:58 +0000

    Slight changes to extract year range script

--- data/unsup/24h/extract_year_range.py
+++ data/unsup/24h/extract_year_range.py
@@ -1,33 +1,34 @@
 import pandas as pd
 import sys
+import datetime
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
 s_year = sys.argv[3]
 e_year = sys.argv[4]
 
-print(type(s_year))
 
 df = pd.read_csv(infile,  lineterminator='\n', parse_dates=['created_date', 'last_change'])
 print(df.shape)
 
+df = df.dropna(subset = ["created_date"])
+print(df.shape)
 
-of = open(outfile, "w")
 ok, notok = 0, 0
 
-for year in range(int(s_year),int(e_year)+1):
-    df = df[df.created_date.str.startswith(year, na = False)]
-    print(year, df.shape)
 
+with  open(outfile, "w") as of:
+    for year in range(int(s_year),int(e_year)+1):
+        df_year = df[(df.created_date >= datetime.datetime(year = year, month = 1, day = 1)) & (df.created_date <= datetime.datetime(year = year, month = 12, day = 31))]
+        print(year, df_year.shape)
 
-    for t in df.content.tolist():
-        try:
-            text = t.replace("\n", " ").replace("\r", " ") + "\n"
-            of.write(text)
-            ok += 1
-        except:
-            notok += 1
+        for t in df_year.content.tolist():
+            try:
+                text = t.replace("\n", " ").replace("\r", " ") + "\n"
+                of.write(text)
+                ok += 1
+            except:
+                notok += 1
 
 print("Done! %d / %d %s Ok," % (ok, ok + notok, outfile))
-of.close()
 
