cda5a91cd02a196d199d5e327ae03a140b4b98f4
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-02-16 06:03:01 +0000

    checkpoints need to fix old code

--- model/unsup-scripts/run_vocab_classification.py
+++ model/unsup-scripts/run_vocab_classification.py
@@ -10,8 +10,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Model
     parser.add_argument("--berts", nargs="+", default=["mbert", "csebert"])
+     parser.add_argument("-all_steps", action='store_true',
+                        help='To Train on all steps check point')
     args = parser.parse_args()
     berts = args.berts
+    all_steps = agrs.all_steps
 
     # berts = ["mbert", "csebert"]
     # berts = ["csebert"]
@@ -33,23 +36,28 @@ if __name__ == "__main__":
             for dataset_type in dataset_types_LM:
                 for dataset in datasets:
                     for vocab_init_type in vocab_init_types:
-                        model_dir_str = bert + '_finetune_' + dataset_size + '_' + dataset_type + '_vocab' + '_' + vocab_init_type
-                        model_card = '../output/' + model_dir_str
 
-                        output_dir = '../results/classify/' + model_dir_str +'_'+dataset
-                        logging_dir = '../logs/classify/' + model_dir_str+'_'+dataset
+                        if all_steps:
+                            checkpoints = ['/checkpoint-500','/checkpoint-1000','/checkpoint-1500']
+                        
+                        for checkpoint in checkpoints:
+                            model_dir_str = bert + '_finetune_' + dataset_size + '_' + dataset_type + '_vocab' + '_' + vocab_init_type+checkpoint
+                            model_card = '../output/' + model_dir_str
 
-                        # Recursively create Directory, even if it exits
-                        Path(output_dir).mkdir(parents=True, exist_ok=True)
-                        Path(logging_dir).mkdir(parents=True, exist_ok=True)
-                        list_arg = [
-                            python_path, "train_classification_vocab.py",
-                            "--output_dir", output_dir,
-                            "--logging_dir", logging_dir,
-                            "--model_card", model_card,
-                            "--dataset", dataset
+                            output_dir = '../results/classify/' + model_dir_str +'_'+dataset
+                            logging_dir = '../logs/classify/' + model_dir_str+'_'+dataset
 
-                        ]
+                            # Recursively create Directory, even if it exits
+                            Path(output_dir).mkdir(parents=True, exist_ok=True)
+                            Path(logging_dir).mkdir(parents=True, exist_ok=True)
+                            list_arg = [
+                                python_path, "train_classification_vocab.py",
+                                "--output_dir", output_dir,
+                                "--logging_dir", logging_dir,
+                                "--model_card", model_card,
+                                "--dataset", dataset
 
-                        print(list_arg)
-                        subprocess.call(list_arg)
+                            ]
+
+                            print(list_arg)
+                            subprocess.call(list_arg)
